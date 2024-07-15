dccc08058d975d75b6996cdd9aa22ab1eae89ed2
RafaelMEA <rafaelmartineaquino@gmail.com>
2024-07-15 21:12:28 +0800

    orderscreen

--- mobile/app/screens/OrdersScreen.js
+++ mobile/app/screens/OrdersScreen.js
@@ -3,6 +3,9 @@ import { createMaterialTopTabNavigator } from '@react-navigation/material-top-ta
 import { NavigationContainer } from '@react-navigation/native';
 import ToPayScreen from './ToPayScreen';
 import ToShipScreen from './ToShipScreen';
+import ToRecieveScreen from './ToRecieveScreen';
+import CompletedScreen from './CompletedScreen';
+import CancelledScreen from './CancelledScreen';
 
 function OrdersScreen(){
     const Tab = createMaterialTopTabNavigator()
@@ -11,6 +14,9 @@ function OrdersScreen(){
             <Tab.Navigator>
                 <Tab.Screen name='To Pay' component={ToPayScreen}/>
                 <Tab.Screen name='To Ship' component={ToShipScreen}/>
+                <Tab.Screen name='To Recieve' component={ToRecieveScreen}/>
+                <Tab.Screen name='Completed' component={CompletedScreen}/>
+                <Tab.Screen name='Cancelled' component={CancelledScreen}/>
             </Tab.Navigator>
         </NavigationContainer>
     )
---
+++ mobile/app/screens/ToRecieveScreen.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import {View} from 'react-native'
+
+function ToRecieveScreen(){
+    return(
+        <View></View>
+    )
+}
+
+export default ToRecieveScreen
