0ff27a67fc584ef6199661e90c9c2101146351c8
mikel <mijocalalo@gmail.com>
2024-07-16 17:41:33 +0800

    Converted CSS to NATIVEWIND

--- mobile/tailwind.config.js
+++ mobile/tailwind.config.js
@@ -1,12 +1,18 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./App.{js,jsx,ts,tsx}",
     "./app/screens/**/*.{js,jsx,ts,tsx}",
-    "./app/screens/**/*.{js,jsx,ts,tsx}",
+    "./app/components/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      height: {
+        '50': '200px', 
+      },
+      padding: {
+        '3.5': '14px', 
+      },
+    },
   },
   plugins: [],
 };
