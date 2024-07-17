049646bf174830f3d8be28e9a500d5554d3da52e
RafaelMEA <rafaelmartineaquino@gmail.com>
2024-07-17 23:51:26 +0800

    splash

---
+++ mobile/app/screens/SplashScreen.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Image, Text} from 'react-native';
+import ehh from "../assets/ehh.png";
+
+function SplashScreen() {
+  return (
+    <View className = 'flex-1 justify-center items-center bg-green-600'>
+      <Image source={ehh} className='w-50 h-70' />
+    </View>
+  );
+}
+
+export default SplashScreen;
