5d26f39c052b913b3224e3b97ba8b7c89b1c82fb
RIE ZHENZY ZUMEL <20208094@s.ubaguio.edu>
2024-09-07 02:33:28 +0800

    minor css update

--- frontend/src/components/Sidebar/SidebarTemplates/SidebarItem.jsx
+++ frontend/src/components/Sidebar/SidebarTemplates/SidebarItem.jsx
@@ -3,7 +3,7 @@ import { NavLink } from 'react-router-dom';
 const SidebarItem = ({ to, icon: Icon, text, onClick, isButton }) => {
   const Component = isButton ? 'button' : NavLink;
   return (
-    <Component to={to} className="sidebar-item w-full" onClick={onClick}>
+    <Component to={to} className="sidebar-item" onClick={onClick}>
       <Icon className="sidebar-icon" />
       <span className="sidebar-text">{text}</span>
     </Component>
