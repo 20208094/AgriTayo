346fc4864d92f16ad6778b5619ffe5ea3bffb8d2
RafaelMEA <rafaelmartineaquino@gmail.com>
2024-10-05 02:51:25 +0800

    it's finished but the select location does not have screen yet to navigate

--- mobile/app/screens/farmers/Shop/ViewShopScreen.js
+++ mobile/app/screens/farmers/Shop/ViewShopScreen.js
@@ -8,7 +8,7 @@ import {
   Text,
   Modal,
   Pressable,
-  ScrollView
+  ScrollView,
 } from "react-native";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import * as ImagePicker from "expo-image-picker";
@@ -25,16 +25,16 @@ function ViewShopScreen({ navigation }) {
   const [shopDescription, setShopDescription] = useState("");
   const [shopImage, setShopImage] = useState(null);
   const [modalVisible, setModalVisible] = useState(false);
+  const [shopDeliveryFee, setShopDeliveryFee] = useState("");
+  const [pickupAreaFee, setPickUpAreaFee] = useState("");
 
-  const [isVisibleDelivery, setIsVisibleDelivery] = useState(false);
   const [isCheckedDelivery, setIsCheckedDelivery] = useState(false);
 
-  const [isVisiblePickup, setIsVisiblePickUp] = useState(false)
-  const [isCheckedPickup, setIsCheckedPickup] = useState(false)
+  const [isCheckedPickup, setIsCheckedPickup] = useState(false);
 
-  const [isCheckedCod, setIsCheckedCod] = useState(false)
-  const [isCheckedGcash, setIsCheckedGcash] = useState(false)
-  const [isCheckedBankTransfer, setIsCheckedBankTransfer] = useState(false)
+  const [isCheckedCod, setIsCheckedCod] = useState(false);
+  const [isCheckedGcash, setIsCheckedGcash] = useState(false);
+  const [isCheckedBankTransfer, setIsCheckedBankTransfer] = useState(false);
 
   const formData = new FormData();
 
@@ -86,28 +86,43 @@ function ViewShopScreen({ navigation }) {
 
   const getAsyncShopData = async () => {
     try {
-      const storedData = await AsyncStorage.getItem("shopData");
-      if (storedData) {
-        const parsedData = JSON.parse(storedData);
-        if (Array.isArray(parsedData)) {
-          const shop = parsedData[0];
-          setShopData(shop);
-          setShopName(shop.shop_name);
-          setShopAddress(shop.shop_address);
-          setShopDescription(shop.shop_description);
-          setShopImage(shop.shop_image_url);
-        } else {
-          setShopData(parsedData);
-          setShopName(parsedData.shop_name);
-          setShopAddress(parsedData.shop_address);
-          setShopDescription(parsedData.shop_description);
-          setShopImage(parsedData.shop_image_url);
+        const storedData = await AsyncStorage.getItem("shopData");
+        if (storedData) {
+            const parsedData = JSON.parse(storedData);
+            console.log('Parsed Data:', parsedData);  // Debugging log
+            if (Array.isArray(parsedData)) {
+                const shop = parsedData[0];
+                setShopData(shop);
+                setShopName(shop.shop_name);
+                setShopAddress(shop.shop_address);
+                setShopDescription(shop.shop_description);
+                setShopImage(shop.shop_image_url);
+                setShopDeliveryFee(String(shop.delivery_price));
+                setPickUpAreaFee(String(shop.pickup_price));
+                setIsCheckedPickup(shop.pickup)
+                setIsCheckedDelivery(shop.delivery)
+                setIsCheckedCod(shop.cod)
+                setIsCheckedGcash(shop.gcash)
+                setIsCheckedBankTransfer(shop.bank)
+            } else {
+                setShopData(parsedData);
+                setShopName(parsedData.shop_name);
+                setShopAddress(parsedData.shop_address);
+                setShopDescription(parsedData.shop_description);
+                setShopImage(parsedData.shop_image_url);
+                setShopDeliveryFee(String(parsedData.delivery_price));
+                setPickUpAreaFee(String(parsedData.pickup_price));
+                setIsCheckedPickup(parsedData.pickup)
+                setIsCheckedDelivery(parsedData.delivery)
+                setIsCheckedCod(parsedData.cod)
+                setIsCheckedGcash(parsedData.gcash)
+                setIsCheckedBankTransfer(parsedData.bank)
+            }
         }
-      }
     } catch (error) {
-      console.error("Failed to load shop data:", error);
+        console.error("Failed to load shop data:", error);
     }
-  };
+};
 
   // Use useFocusEffect to re-fetch data when the screen is focused (navigated back)
   useFocusEffect(
@@ -117,155 +132,216 @@ function ViewShopScreen({ navigation }) {
     }, [])
   );
 
-  const handleDelivery = () => {
-    setIsVisibleDelivery(!isVisibleDelivery);
-  };
+  const CustomCheckbox = ({ label, checked, onPress }) => (
+    <TouchableOpacity
+      onPress={onPress}
+      style={{ flexDirection: "row", alignItems: "center", marginVertical: 4 }}
+    >
+      <View
+        style={{
+          width: 24,
+          height: 24,
+          borderRadius: 4,
+          borderWidth: 2,
+          borderColor: checked ? "#00b251" : "#ccc",
+          backgroundColor: checked ? "#00b251" : "transparent",
+          justifyContent: "center",
+          alignItems: "center",
+          marginRight: 8,
+        }}
+      >
+        {checked && <Ionicons name="checkmark" size={16} color="white" />}
+      </View>
+      <Text style={{ fontSize: 16, color: "#333" }}>{label}</Text>
+    </TouchableOpacity>
+  );
 
-  const handlePickup = () => {
-    setIsVisiblePickUp(!isVisiblePickup)
-  }
-
-  const handleSubmit = async () => {
-
-  }
+  const handleSubmit = async () => {};
   return (
-  <SafeAreaView className="flex-1 bg-gray-100">
-    <ScrollView className="flex-1 px-4">
-      <View className="bg-white p-4 rounded-lg shadow-sm relative">
-        {/* Circular frame with shop image */}
-        <View className="items-center mb-4 mt-8">
-          <View className="relative w-24 h-24 rounded-full border-4 border-green-500 shadow-lg bg-white">
-            <Image
-              source={{ uri: shopImage }}
-              className="w-full h-full rounded-full"
+    <SafeAreaView className="flex-1 bg-gray-100">
+      <ScrollView className="flex-1 px-4">
+        <View className="bg-white p-4 rounded-lg shadow-sm relative">
+          {/* Circular frame with shop image */}
+          <View className="items-center mb-4 mt-8">
+            <View className="relative w-24 h-24 rounded-full border-4 border-green-500 shadow-lg bg-white">
+              <Image
+                source={{ uri: shopImage }}
+                className="w-full h-full rounded-full"
+              />
+              <TouchableOpacity
+                className="absolute bottom-0 right-0 bg-green-500 p-2 rounded-full"
+                onPress={() => setModalVisible(true)}
+              >
+                <Ionicons name="pencil" size={20} color="white" />
+              </TouchableOpacity>
+            </View>
+          </View>
+
+          {/* Shop Name */}
+          <View className="mb-4">
+            <Text className="text-base text-gray-600">Shop Name</Text>
+            <TextInput
+              value={shopName}
+              onChangeText={setShopName}
+              className="mt-1 text-lg text-gray-900 border border-gray-300 rounded-lg p-2"
             />
+          </View>
+
+          {/* Shop Address */}
+          <View className="mb-4">
+            <Text className="text-base text-gray-600">Address</Text>
+            <TextInput
+              value={shopAddress}
+              onChangeText={setShopAddress}
+              className="mt-1 text-lg text-gray-900 border border-gray-300 rounded-lg p-2"
+            />
+          </View>
+
+          {/* Shop Location */}
+          <View className="mb-4">
+          <Text className="text-base text-gray-600">Shop Location</Text>
             <TouchableOpacity
-              className="absolute bottom-0 right-0 bg-green-500 p-2 rounded-full"
-              onPress={() => setModalVisible(true)}
+              className="flex-row justify-between items-center p-3 border border-gray-300 rounded-lg"
+              onPress={() => navigation.navigate("")}
             >
-              <Ionicons name="pencil" size={20} color="white" />
+              <Text className="text-base text-gray-600">Select Location</Text>
+              <Ionicons name="chevron-forward-outline" size={20} color="gray" />
+            </TouchableOpacity>
+          </View>
+
+          {/* Shop Description */}
+          <View className="mb-4">
+            <Text className="text-base text-gray-600">Shop Description</Text>
+            <TextInput
+              value={shopDescription}
+              onChangeText={setShopDescription}
+              className="mt-1 text-lg text-gray-900 border border-gray-300 rounded-lg p-2"
+            />
+          </View>
+
+          {/* Shipping Options */}
+          <View className="mb-4">
+            <Text className="text-base text-gray-600">Shipping Options: </Text>
+
+            <CustomCheckbox
+              label="Delivery"
+              checked={isCheckedDelivery}
+              onPress={() => setIsCheckedDelivery(!isCheckedDelivery)}
+            />
+            {isCheckedDelivery && (
+              <>
+                <Text className="text-base text-gray-600">Delivery Fee:</Text>
+                <TextInput
+                keyboardType="numeric"
+                  value={shopDeliveryFee}
+                  onChangeText={setShopDeliveryFee}
+                  className="mt-1 text-lg text-gray-900 border border-gray-300 rounded-lg p-2"
+                /> 
+              </>
+            )}
+
+            <CustomCheckbox
+              label="Pickup"
+              checked={isCheckedPickup}
+              onPress={() => setIsCheckedPickup(!isCheckedPickup)}
+            />
+            {isCheckedPickup && (
+              <>
+                <Text className="text-base text-gray-600">Pickup Address:</Text>
+                <TouchableOpacity
+                  className="flex-row justify-between items-center p-3 border border-gray-300 rounded-lg"
+                  onPress={() => navigation.navigate("")}
+                >
+                  <Text className="text-base text-gray-600">
+                    Select Location
+                  </Text>
+                  <Ionicons
+                    name="chevron-forward-outline"
+                    size={20}
+                    color="gray"
+                  />
+                </TouchableOpacity>
+                <Text className="text-base text-gray-600">
+                  Pickup Area Fee:
+                </Text>
+                <TextInput
+                  keyboardType="numeric"
+                  value={pickupAreaFee}
+                  onChangeText={setPickUpAreaFee}
+                  className="mt-1 text-lg text-gray-900 border border-gray-300 rounded-lg p-2"
+                />
+              </>
+            )}
+          </View>
+
+          {/* Available Payment Method */}
+          <View className="mb-4">
+            <Text className="text-base text-gray-600">
+              Available Payment Methods
+            </Text>
+
+            <CustomCheckbox
+              label="Cash on Delivery (COD)"
+              checked={isCheckedCod}
+              onPress={() => setIsCheckedCod(!isCheckedCod)}
+            />
+
+            <CustomCheckbox
+              label="GCash"
+              checked={isCheckedGcash}
+              onPress={() => setIsCheckedGcash(!isCheckedGcash)}
+            />
+
+            <CustomCheckbox
+              label="Bank Transfer"
+              checked={isCheckedBankTransfer}
+              onPress={() => setIsCheckedBankTransfer(!isCheckedBankTransfer)}
+            />
+          </View>
+
+          {/* Submit Button */}
+          <TouchableOpacity
+            className="bg-green-500 py-3 rounded-lg flex-row justify-center items-center shadow-lg mt-4 mb-4"
+            onPress={handleSubmit}
+          >
+            <Text className="text-lg text-white">Submit</Text>
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
+
+      {/* Modal for user gallery */}
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => setModalVisible(false)}
+      >
+        <View className="flex-1 justify-center items-center bg-black bg-opacity-50">
+          <View className="bg-white p-6 rounded-lg shadow-lg w-3/4">
+            <Text className="text-lg font-semibold text-gray-900">
+              Update Profile Picture
+            </Text>
+            <TouchableOpacity
+              className="mt-4 p-4 bg-green-500 rounded-lg flex-row justify-center items-center"
+              onPress={selectImageFromGallery}
+            >
+              <Ionicons name="image-outline" size={24} color="white" />
+              <Text className="text-lg text-white ml-2">
+                Select from Gallery
+              </Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              className="mt-4 p-4 bg-red-500 rounded-lg flex-row justify-center items-center"
+              onPress={() => setModalVisible(false)}
+            >
+              <Ionicons name="close-outline" size={24} color="white" />
+              <Text className="text-lg text-white ml-2">Cancel</Text>
             </TouchableOpacity>
           </View>
         </View>
-
-        {/* Shop Name */}
-        <View className="mb-4">
-          <Text className="text-base text-gray-600">Shop Name</Text>
-          <TextInput
-            value={shopName}
-            onChangeText={setShopName}
-            className="mt-1 text-lg text-gray-900 border border-gray-300 rounded-lg p-2"
-          />
-        </View>
-
-        {/* Shop Address */}
-        <View className="mb-4">
-          <Text className="text-base text-gray-600">Address</Text>
-          <TextInput
-            value={shopAddress}
-            onChangeText={setShopAddress}
-            className="mt-1 text-lg text-gray-900 border border-gray-300 rounded-lg p-2"
-          />
-        </View>
-
-        {/* Shop Location */}
-        <View className="mb-4">
-          <TouchableOpacity onPress={() => navigation.navigate("")}>
-            <Text className="text-base text-gray-600">Shop Location</Text>
-          </TouchableOpacity>
-        </View>
-
-        {/* Shop Description */}
-        <View className="mb-4">
-          <Text className="text-base text-gray-600">Shop Description</Text>
-          <TextInput
-            value={shopDescription}
-            onChangeText={setShopDescription}
-            className="mt-1 text-lg text-gray-900 border border-gray-300 rounded-lg p-2"
-          />
-        </View>
-
-        {/* Shipping Options */}
-        <View className="mb-4">
-          <Text className="text-base text-gray-600">Shipping Options</Text>
-          <TouchableOpacity onPress={handleDelivery}>
-            <Text className="mt-1 text-lg text-gray-900">Delivery</Text>
-          </TouchableOpacity>
-          {isVisibleDelivery && (
-            <Pressable onPress={() => setIsCheckedDelivery(!isCheckedDelivery)}>
-              <Text>{isCheckedDelivery ? "☑ " : "☐ "}Delivery Fee</Text>
-            </Pressable>
-          )}
-        </View>
-
-        <View className="mb-4">
-          <TouchableOpacity onPress={handlePickup}>
-            <Text className="mt-1 text-lg text-gray-900">Pickup</Text>
-          </TouchableOpacity>
-          {isVisiblePickup && (
-            <Pressable onPress={() => setIsCheckedPickup(!isCheckedPickup)}>
-              <Text>{isCheckedPickup ? "☑ " : "☐ "}Pickup Address</Text>
-            </Pressable>
-          )}
-        </View>
-
-        {/* Available Payment Method */}
-        <View className="mb-4">
-          <Text className="text-base text-gray-600">Available Payment Methods</Text>
-          <Pressable onPress={() => setIsCheckedCod(!isCheckedCod)}>
-            <Text>{isCheckedCod ? "☑ " : "☐ "} Cash on Delivery (COD)</Text>
-          </Pressable>
-          <Pressable onPress={() => setIsCheckedGcash(!isCheckedGcash)}>
-            <Text>{isCheckedGcash ? "☑ " : "☐ "} GCash</Text>
-          </Pressable>
-          <Pressable onPress={() => setIsCheckedBankTransfer(!isCheckedBankTransfer)}>
-            <Text>{isCheckedBankTransfer ? "☑ " : "☐ "}Bank Transfer</Text>
-          </Pressable>
-        </View>
-
-        {/* Submit Button */}
-        <TouchableOpacity
-          className="bg-green-500 py-3 rounded-lg flex-row justify-center items-center shadow-lg mt-4 mb-4"
-          onPress={handleSubmit}
-        >
-          <Text className="text-lg text-white">Submit</Text>
-        </TouchableOpacity>
-      </View>
-    </ScrollView>
-
-    {/* Modal for user gallery */}
-    <Modal
-      animationType="slide"
-      transparent={true}
-      visible={modalVisible}
-      onRequestClose={() => setModalVisible(false)}
-    >
-      <View className="flex-1 justify-center items-center bg-black bg-opacity-50">
-        <View className="bg-white p-6 rounded-lg shadow-lg w-3/4">
-          <Text className="text-lg font-semibold text-gray-900">
-            Update Profile Picture
-          </Text>
-          <TouchableOpacity
-            className="mt-4 p-4 bg-green-500 rounded-lg flex-row justify-center items-center"
-            onPress={selectImageFromGallery}
-          >
-            <Ionicons name="image-outline" size={24} color="white" />
-            <Text className="text-lg text-white ml-2">
-              Select from Gallery
-            </Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            className="mt-4 p-4 bg-red-500 rounded-lg flex-row justify-center items-center"
-            onPress={() => setModalVisible(false)}
-          >
-            <Ionicons name="close-outline" size={24} color="white" />
-            <Text className="text-lg text-white ml-2">Cancel</Text>
-          </TouchableOpacity>
-        </View>
-      </View>
-    </Modal>
-  </SafeAreaView>
-);
+      </Modal>
+    </SafeAreaView>
+  );
 }
 
 export default ViewShopScreen;
